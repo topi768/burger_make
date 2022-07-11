71b1cc90d7a843d17f559dc851e8e18dd51f6b35
topi768 <90519641+topi768@users.noreply.github.com>
2022-07-11 20:55:38 +1000

    use width/height

--- src/pages/Discover.vue
+++ src/pages/Discover.vue
@@ -4,20 +4,54 @@
         <h1 class="title">Make Your Burger</h1>
         <img class="main-img" src="@/assets/img/discover/mainImg.png" alt="">
     </div>
-    <button class="button">
+    <button class="button" :style="{ width: width, height: height}" v-on:mouseenter="buttonAnimation" >
         <p>make burger</p>
     </button>
-
 </div>
 
     
 </template>
 
 <script>
+
 export default {
     beforeCreate: function() {
         document.body.className = 'discover';
+    },
+      data() {
+        return {
+            width: '120px',
+            height: '120px'
+        };
+    },
+    // methods: {
+    //     buttonAnimation() {
+    //         let button = event.target
+    //         // startWidth = button.offsetWidth
+    //         // endWidth= startWidth + 
+    //         console.log(startWidth);
+    //     }
+
+    // },
+    computed: {
+        buttonAnimation() {
+            let button = event.target
+            let startWidth =button.offsetWidth
+            let endWidth= startWidth + 60
+
+            button.style.width = button.offsetWidth
+
+            for (startWidth; startWidth < 220; startWidth++) {
+                this.width = startWidth + 'px'
+                this.height = startWidth + 'px'
+                console.log("sr");
+            }
+
+            console.log(button.getBoundingClientRect().width);
+        }
     }
+
+
 }
 </script>
 
@@ -71,36 +105,42 @@ export default {
     font-size: 1.5em;
     line-height: 30px;
     text-transform: uppercase;
-    //
-    animation-duration: 0.8s;
-      
-        @keyframes example {
-            // 0% {
-            //     // transform: scale(1);
-            // }
-        // 15% {
-        //     transform: scale(1.3);
-        // }
-        // 30% {
-        //     transform: scale(1.5);
-        // }
-        // 40% {
-        //     transform: scale(1.2);
-        // }
-        // 50% {
-        //     transform: scale(1.3);
-        // }
-        75% {
-            transform: scale(1.5);
+    // 
+    // animation-duration: 0.8s; 
+    // animation-fill-mode: both; 
+    // transform: scale(1);
+    //     @keyframes example {
+    //         // 0% {
+    //         //     // transform: scale(1);
+    //         // }
+    //     // 15% {
+    //     //     transform: scale(1.3);
+    //     // }
+    //     // 30% {
+    //     //     transform: scale(1.5);
+    //     // }
+    //     // 40% {
+    //     //     transform: scale(1.2);
+    //     // }
+    //     // 50% {
+    //     //     transform: scale(1.3);
+    //     // }
+    //     // 75% {
+    //     //     transform: scale(1.5);
 
-        }
-        100% {
-            transform: scale(1.4);
-        }
-    }
+    //     // }
+    //     100% {
+    //         transform: scale(1.4);
+    //     }
+    // }
+    // transform: scale(1);
+
+        // transition: 0.3s;
+        // animation:  0.8s ease;
+        // animation-fill-mode: forwards;
     &:hover {
-        animation-name: example;
-        animation: example 0.8s ease;
+        
+        // transform: scale(1.5);
         // animation-duration: 0.5s;
     }
 }
