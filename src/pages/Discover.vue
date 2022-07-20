12d7e062f36f66e041f95b5aaef76c878be900fc
topi768 <90519641+topi768@users.noreply.github.com>
2022-07-20 13:02:23 +1000

    main page ready

--- src/pages/Discover.vue
+++ src/pages/Discover.vue
@@ -1,14 +1,17 @@
 <template>
 <div class="container">
-    <div class="flex-container">
-        <h1 class="title">Make Your Burger</h1>
-        <img class="main-img" src="@/assets/img/discover/mainImg.png" alt="">
-        <button class="button" @click="$router.push('/makeBurger')" >
-            <p>make burger</p>
-        </button>
+    <div class="content" >
+        <div class="flex-container" >
+            <h1 class="title">Make Your Burger</h1>
+            <div class="img__wrapper">
+                <img class="main-img" src="@/assets/img/discover/mainImg.png" alt="">
+                <button class="button" @click="$router.push('/makeBurger')" >
+                    <p>make burger</p>
+                </button>
+            </div>
+        </div>
     </div>
-
-      <router-view/>
+    <router-view/>
 
 </div>
 
@@ -18,27 +21,6 @@
 <script>
 
 export default {
-    // beforeCreate: function() {
-    //     document.body.className = 'discover';
-    // },
-      data() {
-        return {
-            
-        };
-    },
-    // methods: {
-    //     buttonAnimation() {
-    //         let button = event.target
-    //         // startWidth = button.offsetWidth
-    //         // endWidth= startWidth + 
-    //         console.log(startWidth);
-    //     }
-
-    // },
-    computed: {
-
-    }
-
 
 }
 </script>
@@ -46,73 +28,91 @@ export default {
 <style lang="scss" scoped>
 @import "@/assets/variables.scss";
 
-.flex-container {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    flex-wrap: wrap;
-    .main-img {
-        width: 75%;
-        max-width: 980px;
-        max-height: 930px;
-        z-index: 0;
-    }
-    .title {
-        display: block;
-        width: 1px;
-        font-family: 'OpenSans Bold';
-        font-style: normal;
-    
-        font-size: 4.5rem;
-        line-height: 80px;  
-        color: $black;
-        z-index: 1;
-    }
-    @media(max-width: 992px){
-        flex-direction: column;
-        .title {
-            width: 100%;
-            font-size: 3rem;
-        }
-        .main-img{
-            position: relative;
-
-        }
-
-    }
-    .button {
-        position: absolute;
-        top: 65%;
-        left: 33%;
-        border-radius: 50%;
-        width: 9.375vw;
-        height: 9.375vw;
+.content {
+    .flex-container {
         display: flex;
+        justify-content: space-between;
         align-items: center;
-        justify-content: center;
-        min-width: 120px;
-        min-height: 120px;
-        background: $primary;
-        border: 0px;
-        cursor: pointer;
-        //text
-        color: $white;
-        font-family: 'OpenSans Bold';
-        font-style: normal;
-        font-weight: 800;
-        font-size: 1.5rem;
-        line-height: 30px;
-        text-transform: uppercase;
-        transition: 0.3s;
-        &:hover { 
-            transform: scale(1.5);
+        flex-wrap: wrap;
+        // overflow: hidden;
+        @media(max-width: 992px){
+            flex-direction: column;
         }
-        @media(max-width: 992px) {
-            position: absolute;
-            top: 50%;
-            left: 50%;
+        .title {
+            display: block;
+            width: 1px;
+            font-family: 'OpenSans Regular';
+            font-style: normal;
+            font-size: 4.5rem;
+            line-height: 80px;  
+            color: $black;
+            z-index: 1;
+            @media(max-width: 992px){
+                width: 100%;
+                font-size: 3rem;
+                line-height: 58px;
+            }
+        }
+        .img__wrapper {
+            width: 63%;
+            position: relative;
+            @media(max-width: 992px) {
+                width: 100%;
+                height: 100%;
+            }
+            .button{
+                position: absolute;
+                top: 60%;
+                left: -10%;
+                border-radius: 50%;
+                width: 9.375vw;
+                height: 9.375vw;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                min-width: 120px;
+                min-height: 120px;
+                background: $primary;
+                border: 0px;
+                cursor: pointer;
+                transition: 0.3s;
+                //text
+                color: $white;
+                font-family: 'OpenSans ExtraBold';
+                font-style: normal;
+                font-weight: 800;
+                font-size: 1.5rem;
+                line-height: 30px;
+                text-transform: uppercase;
+                &:hover { 
+                    transform: scale(1.5);
+                    @media(max-width: 992px) {
+                        transform: scale(1.5) translate(-25%, -25%);
+                    }
+                }
+                @media(max-width: 992px) {
+                    top: 50%;
+                    left: 50%;
+                    margin-right: -50%;
+                    transform: translate(-50%, -50%);
+                    font-size: 1rem;
+                    line-height: 20px;
+                }
+            }
+            .main-img {
+                width: 100%;
+                height: 100%;
+            }
         }
     }
 }
+
+
+
+
+.button {
+
+
+    }
 
 </style>
